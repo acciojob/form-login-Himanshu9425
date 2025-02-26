624f55ed8ea7727adddb0f016912f02562b814e5
Himanshu9425 <147365994+Himanshu9425@users.noreply.github.com>
2025-02-26 14:38:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	let first=document.querySelector("#first1");
+	let last=document.querySelector("#second");
+	let message=first.value+" "+last.value;
+alert(message);
 }
